3d5aa5c7469d0ac8a36869e3dc28b46220afeb34
Renzo Santillán Chavez <rsantillanc@gmail.com>
2022-10-14 20:52:06 -0500

    ejercicio

---
+++ eljercicio-js-02.js
@@ -0,0 +1,10 @@
+//Notation
+list = ["Renzo",33,true,new Date("7/19/1989")]
+book = {
+    "title":'Biblia',
+    author: "God",
+    date:new Date(),
+    url:"https//www.bibliagateway.com"
+}
+list.push(book)
+console.log(list)
